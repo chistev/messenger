8c35a54844208f7f54e06f72a547a71476c066f5
chistev <stephenowabie@gmail.com>
2024-07-01 15:59:55 +0100

    added Settings component

---
+++ client/src/components/Settings.svelte
@@ -0,0 +1,29 @@
+<style>
+  .border-right {
+    border-right: 1px solid #2f3336;
+    border-left: 1px solid #2f3336;
+  }
+</style>
+
+<div id="content-column" class="col-md-8 border-right">
+    <div class="p-3">
+        <div class="d-flex">
+            <div>
+                <i class="bi bi-arrow-left fs-4" id="back-icon" style="cursor: pointer;"></i>
+            </div>
+            <div>
+                <h3 class="mb-5 ms-5" style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif; font-weight: 800; font-size: 31px; line-height: 36px; color: #e7e9ea;">Direct Messages</h3>
+            </div>
+        </div>
+        <div class="d-flex align-items-center justify-content-between">
+            <div>
+                <div style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif; font-weight: 400; font-size: 15px; line-height: 20px; color: #e7e9ea;">Show read receipts</div>
+            </div>
+            <div>
+                <input type="checkbox" name="read-receipts" value="read-receipts">
+            </div>
+        </div>
+        <div>Let people you’re messaging with know when you’ve seen their messages.</div>
+    </div>
+</div>
+
