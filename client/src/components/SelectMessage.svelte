3179b92ec2eaa45f3b9dd3a58b9eba6171b279a0
chistev <stephenowabie@gmail.com>
2024-07-03 21:45:50 +0100

    moved the welcome message component in the chat content column to its own component

---
+++ client/src/components/SelectMessage.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let toggleMessageModal;
+</script>
+
+<div id="content-column" class="col-md-8 border-right d-flex flex-column align-items-center justify-content-center">
+    <div class="p-3 text-center">
+        <h3 style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif; font-weight: 800; font-size: 31px; line-height: 36px; color: #e7e9ea;">
+            Select a message
+        </h3>
+        <p style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif; font-weight: 400; font-size: 15px; line-height: 20px; color: #71767b;">
+            Choose from your existing conversations, or start a new one.
+        </p>
+        <button class="btn p-3" style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif; font-weight: 700; font-size: 17px; line-height: 20px; color: #ffffff; background-color: #1d9bf0; border-radius: 50px;" on:click={toggleMessageModal}>
+            New message
+        </button>
+    </div>
+</div>
+
+<style>
+    .border-right {
+        border-right: 1px solid #2f3336;
+    }
+</style>
--- client/src/routes/messages/+page.svelte
+++ client/src/routes/messages/+page.svelte
@@ -5,6 +5,7 @@
     import SelectedUsersList from '../../components/SelectedUsersList.svelte';
     import Spinner from '../../components/Spinner.svelte';
     import ChatPanel from '../../components/ChatPanel.svelte';
+    import SelectMessage from '../../components/SelectMessage.svelte'; // Import the new component
     import { onMount } from 'svelte';
 
     let showSettings = false;
@@ -55,7 +56,6 @@
 <style>
     .border-right {
         border-right: 1px solid #2f3336;
-        border-left: 1px solid #2f3336;
     }
 </style>
 
@@ -89,19 +89,7 @@
             {:else if currentChatUser}
                 <ChatPanel {currentChatUser} />
             {:else}
-                <div id="content-column" class="col-md-8 border-right d-flex flex-column align-items-center justify-content-center">
-                    <div class="p-3 text-center">
-                        <h3 style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif; font-weight: 800; font-size: 31px; line-height: 36px; color: #e7e9ea;">
-                            Select a message
-                        </h3>
-                        <p style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif; font-weight: 400; font-size: 15px; line-height: 20px; color: #71767b;">
-                            Choose from your existing conversations, or start a new one.
-                        </p>
-                        <button class="btn p-3" style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif; font-weight: 700; font-size: 17px; line-height: 20px; color: #ffffff; background-color: #1d9bf0; border-radius: 50px;" on:click={toggleMessageModal}>
-                            New message
-                        </button>
-                    </div>
-                </div>
+                <SelectMessage {toggleMessageModal} />
             {/if}
         </div>
     </div>
