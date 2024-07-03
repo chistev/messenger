715eb0936bbdce1a492ce8e540e88ee07adfb59f
chistev <stephenowabie@gmail.com>
2024-07-03 10:21:50 +0100

    moved parts of the messages/+page.svelte html code to the SideBarWelcomeMessage component

--- client/src/routes/messages/+page.svelte
+++ client/src/routes/messages/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import Settings from '../../components/Settings.svelte';
     import MessageModal from '../../components/MessageModal.svelte';
+	import SideBarWelcomeMessage from '../../components/SideBarWelcomeMessage.svelte';
 
     let showSettings = false;
     let settingsComponent;
@@ -163,18 +164,7 @@
                             <i class="bi bi-envelope-at" style="cursor: pointer;" on:click={toggleMessageModal} aria-label="Open Message Modal"></i>
                         </div>
                     </div>
-                    <br id="break-tag">
-                    <div id="welcome-message">
-                        <h4 style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif; font-weight: 800; font-size: 31px; line-height: 36px; color: #e7e9ea;">
-                            Welcome to your inbox!
-                        </h4>
-                        <p style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif; font-weight: 400; font-size: 15px; line-height: 20px; color: #71767b;">
-                            Search for a user and have private conversations with them.
-                        </p>
-                        <button class="btn p-3" style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif; font-weight: 700; font-size: 17px; line-height: 20px; color: #ffffff; background-color: #1d9bf0; border-radius: 50px;" on:click={toggleMessageModal}>
-                            Write a message
-                        </button>
-                    </div>
+                    <SideBarWelcomeMessage on:writeMessage={toggleMessageModal} />
                 </div>
             </div>
             {#if showSettings && settingsComponent}
