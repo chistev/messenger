ca3f47bb62cde3f664eff77b897f6c028ed5a819
chistev <stephenowabie@gmail.com>
2024-07-15 22:13:29 +0100

    changed redirection to home page from the localhost to the actual production url

--- client/src/hooks.server.js
+++ client/src/hooks.server.js
@@ -1,6 +1,6 @@
 export async function handle({ event, resolve }) {
     if (event.url.pathname === '/') {
-      return Response.redirect('http://localhost:5173/messages', 302);
+      return Response.redirect('https://svelte-of1p.onrender.com/messages', 302);
     }
   
     return resolve(event);
