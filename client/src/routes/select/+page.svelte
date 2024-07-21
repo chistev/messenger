62e21742d98ce569c8345972eedc26adf4a82e3b
chistev <stephenowabie@gmail.com>
2024-07-21 21:19:49 +0100

    modified utils.js to retrieve the successfully saved token from localstorage; applied the function in the check-new-user fetch request

--- client/src/routes/select/+page.svelte
+++ client/src/routes/select/+page.svelte
@@ -14,6 +14,7 @@
         console.log('checkUserStatus function called');
 
         const jwtToken = getJwtToken();
+        console.log('Retrieved JWT token:', jwtToken);
         
         const response = await fetch('https://messenger-tu85.onrender.com/api/check-new-user', {
             credentials: 'include',
--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,9 +1,9 @@
 export const getJwtToken = () => {
-    console.log('All cookies:', document.cookie);
-
-    const token = document.cookie.split('; ').find(row => row.startsWith('jwt='));
-    const jwtToken = token ? token.split('=')[1] : null;
+    console.log('Retrieving JWT token from localStorage');
+    
+    const jwtToken = localStorage.getItem('jwt');
     console.log('Retrieved JWT token:', jwtToken);
+    
     return jwtToken;
   };
   
