23524d67d702923db0e6f62def974a0a6a4c119e
chistev <stephenowabie@gmail.com>
2024-07-21 17:14:23 +0100

    modified checkNewUser middleware to determine if the select route should be accessible to the user or not

--- server/middleware/checkNewUser.js
+++ server/middleware/checkNewUser.js
@@ -1,4 +1,5 @@
-const User = require('../models/User');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const checkNewUser = async (req, res, next) => {
   try {
@@ -6,32 +7,31 @@ const checkNewUser = async (req, res, next) => {
     console.log('Request received:', {
       headers: req.headers,
       body: req.body,
-      user: req.user
+      user: req.user,
+      tempUser: req.tempUser
     });
 
-    // Correctly check for req.user.googleId directly
-    if (!req.user || !req.user.googleId) {
-      console.log('Unauthorized: No user or user ID found');
+    // Check if JWT token is present
+    if (!req.user && !req.tempUser) {
+      console.log('Unauthorized: No user or temporary user found');
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    const googleId = req.user.googleId;
-    console.log('Checking existing user with Google ID:', googleId);
-    const existingUser = await User.findOne({ googleId });
+    // If the token is for a fully registered user
+    if (req.user) {
+      console.log('Token belongs to a fully registered user:', req.user);
+      return res.json({ exists: true });
+    }
 
-    if (existingUser) {
-      console.log('Existing user found:', existingUser);
-      if (existingUser.username) {
-        console.log('User has a username');
-        return res.json({ exists: true });
-      } else {
-        console.log('User does not have a username');
-        return res.json({ exists: false });
-      }
-    } else {
-      console.log('No existing user found');
+    // If the token is for a temporary user
+    if (req.tempUser) {
+      console.log('Token belongs to a temporary user:', req.tempUser);
       return res.json({ exists: false });
     }
+
+    // If the token is invalid or does not match any user
+    console.log('Invalid token or user not found');
+    return res.status(401).json({ error: 'Unauthorized' });
   } catch (error) {
     console.error('Error checking user:', error);
     res.status(500).json({ error: 'Server error' });
